d80d6a78d815da882e9b54af20c0531e4bbd7b1c
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-10 14:24:38 -0300

    feat: pagina de edicao com captura de rota e requisicao PUT

--- frontend/gestor-funcionarios-ui/src/app/app-routing.module.ts
+++ frontend/gestor-funcionarios-ui/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EditarComponent } from './pages/editar/editar.component';
 
 
 const routes: Routes = [
+{path: 'editar/:id', component: EditarComponent},
 {path: 'cadastro', component: CadastroComponent},
 {path: '', component: HomeComponent}
 ];
---
+++ frontend/gestor-funcionarios-ui/src/app/pages/editar/editar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Funcionario } from 'src/app/models/Funcionarios';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
+
+@Component({
+  selector: 'app-editar',
+  templateUrl: './editar.component.html',
+  styleUrls: ['./editar.component.css']
+})
+export class EditarComponent implements OnInit {
+
+  btnAcao = "Editar";
+  btnTitulo = "Editar Funcionário!";
+  funcionario!: Funcionario;
+
+  constructor(private funcionarioService: FuncionarioService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.funcionarioService.GetFuncionario(id).subscribe((data) => {
+      this.funcionario = data.dados;
+
+    });
+  }
+
+  editFuncionario(funcionario: Funcionario) {
+
+    this.funcionarioService.EditarFuncionario(funcionario).subscribe(data => {
+      this.router.navigate(['/']);
+    });
+
+  }
+}
