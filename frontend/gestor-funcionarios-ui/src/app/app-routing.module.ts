c5316d70118d3ef555720597f6e19b52ef898f11
Klaudiane Deda <klaudianejdeda@gmail.com>
2026-09-10 11:33:07 -0300

    feat: configuracao de rotas e criacao da pagina de Cadastro

--- frontend/gestor-funcionarios-ui/src/app/app-routing.module.ts
+++ frontend/gestor-funcionarios-ui/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'cadastro', component: CadastroComponent},
+{path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/gestor-funcionarios-ui/src/app/app.module.ts
+++ frontend/gestor-funcionarios-ui/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
